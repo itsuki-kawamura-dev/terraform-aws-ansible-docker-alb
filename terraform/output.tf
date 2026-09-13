c7e210b3dc11f31c7e5b486ccc0bec117affcfb9
Itsuki Kawamura <kawamura.itsuki.work@gmail.com>
2026-09-13 21:16:24 +0100

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,1 +1,11 @@
+########### ALB OUTPUT ###########
+output "alb_dns_name" {
+  description = "ALB DNS name"
+  value       = aws_lb.main.dns_name
+}
 
+########### ALB OUTPUT ###########
+output "ec2_instance_id" {
+  description = "EC2 instance id"
+  value       = aws_instance.main.id
+}
